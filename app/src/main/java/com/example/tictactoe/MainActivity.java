2c80fba8a112d77ea4aa5600863f28a02b27f735
Sapeksh Pareek <sapekshpareek136@gmail.com>
2023-05-11 01:51:08 +0530

    Icon Change

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -6,18 +6,23 @@ import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
  public class MainActivity extends AppCompatActivity {
     int player = 1;
     public void load(View view){
 
         ImageView v =(ImageView) view;
+        int tag = Integer.parseInt(v.getTag().toString());
+
         if(player==1){
             v.setImageResource(R.drawable.cross);
+            Toast.makeText(this,tag+"Cross",Toast.LENGTH_LONG).show();
             player = 0;
         }
         else{
             v.setImageResource(R.drawable.circle);
+            Toast.makeText(this,tag+"Circle",Toast.LENGTH_LONG).show();
             player =1;
         }
 
