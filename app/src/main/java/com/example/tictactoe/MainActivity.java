0d21cacdbaaf5a55d682901fe964d6d630619d02
Sapeksh Pareek <sapekshpareek136@gmail.com>
2023-05-11 01:13:25 +0530

    Cross

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -1,10 +1,20 @@
-package com.example.tictactoe;
+ package com.example.tictactoe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+ public class MainActivity extends AppCompatActivity {
+
+    public void load(View view){
+
+        ImageView v =(ImageView) view;
+        v.setImageResource(R.drawable.cross);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
