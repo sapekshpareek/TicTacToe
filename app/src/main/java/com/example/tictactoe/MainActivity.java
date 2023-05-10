7be820261762e460006169c409629d5f4c843622
Sapeksh Pareek <sapekshpareek136@gmail.com>
2023-05-11 02:52:33 +0530

    Added Toast for Winners

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -1,29 +1,33 @@
  package com.example.tictactoe;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
  public class MainActivity extends AppCompatActivity {
-    int player = 1;
+    int player = 1; //player1 is cross
+     int [][]winningStates = {{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
+     int []gameState={-1,-1,-1,-1,-1,-1,-1,-1,-1};
     public void load(View view){
-
         ImageView v =(ImageView) view;
         int tag = Integer.parseInt(v.getTag().toString());
-
         if(player==1){
             v.setImageResource(R.drawable.cross);
-            Toast.makeText(this,tag+"Cross",Toast.LENGTH_LONG).show();
-            player = 0;
+            gameState[tag]=player;
+            Toast.makeText(this,tag+" Cross",Toast.LENGTH_LONG).show();
+            player = 0; //Circle Player
         }
         else{
             v.setImageResource(R.drawable.circle);
-            Toast.makeText(this,tag+"Circle",Toast.LENGTH_LONG).show();
+            gameState[tag]=player;
+            Toast.makeText(this,tag+" Circle",Toast.LENGTH_LONG).show();
             player =1;
+        }
+        for (int[] winningState : winningStates) {
+            if (gameState[winningState[0]] == gameState[winningState[1]] && gameState[winningState[1]] == gameState[winningState[2]] && gameState[winningState[0]] > -1)
+                Toast.makeText(this, "Winner is " + (player == 0 ? 1 : 0), Toast.LENGTH_SHORT).show();
         }
 
     }
