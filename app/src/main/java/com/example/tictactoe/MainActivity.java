14e755e2b536e9dc5cb05312e4978daadca6e72e
Sapeksh Pareek <sapekshpareek136@gmail.com>
2023-05-13 10:10:18 +0530

    Issue Fixed!

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -7,8 +7,8 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
  public class MainActivity extends AppCompatActivity {
-//     boolean isWinner = false;
-//     int imageClicked = -1;
+     boolean isWinner = false;
+     int imageClicked = -1;
      int player = 1; //player1 is cross
      int [][]winningStates = {{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
      int []gameState={-1,-1,-1,-1,-1,-1,-1,-1,-1};
@@ -17,27 +17,29 @@ import android.widget.Toast;
         ImageView v = (ImageView) view;
 
         int tag = Integer.parseInt(v.getTag().toString());
-//        imageClicked = gameState[tag];
+        imageClicked = gameState[tag];
 //
-//        if(isWinner == false && imageClicked ==-1) {
+        if(isWinner == false && imageClicked ==-1) {
 
             if (player == 1) {
                 v.setImageResource(R.drawable.cross);
                 gameState[tag] = player;
-                Toast.makeText(this, tag + " Cross", Toast.LENGTH_LONG).show();
+                Toast.makeText(this, tag + " Cross", Toast.LENGTH_SHORT).show();
                 player = 0; //Circle Player
             } else {
                 v.setImageResource(R.drawable.circle);
                 gameState[tag] = player;
-                Toast.makeText(this, tag + " Circle", Toast.LENGTH_LONG).show();
+                Toast.makeText(this, tag + " Circle", Toast.LENGTH_SHORT).show();
                 player = 1;
             }
             for (int[] winningState : winningStates) {
-                if (gameState[winningState[0]] == gameState[winningState[1]] && gameState[winningState[1]] == gameState[winningState[2]] && gameState[winningState[0]] > -1)
+                if (gameState[winningState[0]] == gameState[winningState[1]] && gameState[winningState[1]] == gameState[winningState[2]] && gameState[winningState[0]] > -1){
                     Toast.makeText(this, "Winner is " + (player == 0 ? 1 : 0), Toast.LENGTH_SHORT).show();
-//                isWinner = true;
+                isWinner = true;}
             }
-//        }
+//           isWinner = true;
+        }
+
 
     }
 
